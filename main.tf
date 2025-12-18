4d89518812487ad578f222cd5a2adbee1a51a6bc
singh-uma <Umasingh1107@gmail.com>
2025-12-18 14:15:53 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 resource "aws_s3_bucket" "demo" {
   bucket = "uma-terraform-github-actions-demo-ap-south-1"
 }
