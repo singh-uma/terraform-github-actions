902d25209d591a67dd0f44e213b1ee1d8f8a4ef9
Uma Singh <umasingh1107@gmail.com>
2025-12-18 07:02:33 +0000

    Add initial Terraform configuration

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "uma-terraform-github-actions-demo-12345"
+}
+
